81296b3e1d212fcef776e3d8fa203323a75e2a1b
Apoorv Yadav <apoorv2yadav@gmail.com>
2021-05-25 14:06:50 +0530

    removed banner buttons

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "../axios";
 import requests from "../requests";
-import './Banner.css';
+import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -22,8 +22,8 @@ function Banner() {
   //  console.log(movie);
 
   //on long description after n it will show ...
-  function truncate(str, n){
-      return str?.length > n ? str.substr(0, n-1) + "..." : str
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   }
 
   return (
@@ -38,14 +38,14 @@ function Banner() {
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">{movie.title || movie.name || movie.original_name}</h1>
-        <div className="banner__buttons">
+        <h1 className="banner__title">
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+        {/* <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
-        </div>
-        <h1 className="banner__description">
-        {truncate(movie.overview, 150)}
-        </h1>
+        </div> */}
+        <h1 className="banner__description">{truncate(movie.overview, 150)}</h1>
       </div>
       <div className="banner--fadeBottom"></div>
     </header>
